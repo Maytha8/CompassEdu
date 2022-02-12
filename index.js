8e62d20c996c14b574a6b10fdc6adab7b9257e19
Maytham <44939173+Maytha8@users.noreply.github.com>
2022-02-12 12:03:47 +0800

    Remove unneccesary comments

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 'use strict';
 
-/**
- * CompassEdu package
- * @module CompassEdu
- */
-
 const { CompassEdu } = require("./src/CompassEdu")
 
-// Expose root class
 module.exports = CompassEdu;
-
-// Expose individual classes
-//module.exports.CompassEdu    = CompassEdu;
